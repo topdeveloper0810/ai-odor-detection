0e16b08b5666f3ac0c046a46d5599f991cf32758
Lai Deming <laideming935@gmail.com>
2024-03-22 15:09:13 +1000

    modify model

--- ai-nose-model.py
+++ ai-nose-model.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import os
 
 # import seaborn as sns
 from sklearn.tree import DecisionTreeClassifier
@@ -8,51 +10,61 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
-data = pd.read_csv("total.csv")
-data = data.iloc[:, 1:]
-data = data.iloc[:, :-2]
-# print(data.head())
+TRAIN_PATH = "train"
+TEST_PATH = "test"
 
-data["category"] = data["category"].astype("category")
+trainFilePath = os.path.abspath(TRAIN_PATH)
+testFilePath = os.path.abspath(TEST_PATH)
 
-# test = pd.read_csv("test.csv")
-# test = test.iloc[:, 1:]
-# test = test.iloc[:, :-2]
+# Train data
+trainData = pd.read_csv(os.path.join(trainFilePath, "train.csv"))
+trainData = trainData.iloc[:, :-2]
+trainData["category"] = trainData["category"].astype("category")
 
-# print(test_scaled)
+# Test data
+# testData = pd.read_csv(os.path.join(testFilePath, "test.csv"))
+# testData = testData.iloc[:, :-2]
 
-y = data["category"]
-X = data.drop("category", axis=1)
-# print(X.head())
+y = trainData["category"]
+X = trainData.drop("category", axis=1)
 
 # category = {"coffee": 0, "kahlua":1, "lrishCream":2, "rum":3}
 # y_trans = y.map(category)
 # print(y_trans)
 
-# clf = LogisticRegression()
-# clf = DecisionTreeClassifier()
-clf = RandomForestClassifier()
+# model = LogisticRegression()
+# model = DecisionTreeClassifier()
+model = RandomForestClassifier()
+stand = StandardScaler()
 
+# ! Accuracy evaluation
 # X_train, X_test, y_train, y_test = train_test_split(
 #     X, y, test_size=0.2, random_state=42
 # )
 
-stand = StandardScaler()
-X_train = stand.fit_transform(X)
-# X_test = stand.transform(test)
-clf.fit(X_train, y)
-# clf.fit(X_scaled, y)
-# print(X_scaled)
+# X_train = stand.fit_transform(X_train)
+# X_test = stand.transform(X_test)
+# model.fit(X_train, y_train)
 
+# y_pred = model.predict(X_test)
+# print(y_pred)
+# accuracy = accuracy_score(y_test, y_pred)
+# print("Accuracy: ", accuracy)
+
+# ! Test Data
+X = stand.fit_transform(X)
+# testData = stand.transform(testData)
+model.fit(X, y)
+# y_pred = model.predict(testData)
+# print(y_pred)
+
+# ! CLI test
 while True:
-    test = input(">>>input : ")
-    test = test.split(";")[1:-2]
-    # print(test)
-    testData = pd.DataFrame([test], columns=X.columns)
+    odor = input("input : ")
+    odor = odor.split(";")[1:-2]
+    odor = np.array(odor)
+    odor = pd.DataFrame([odor], columns=trainData.columns[1:])
     # print(testData)
-    testData = stand.transform(testData)
-    y_pred = clf.predict(testData)
-    # accuracy = accuracy_score(y_test, y_pred)
-    # print(y_pred)
-    print(">>>>Output : ", y_pred[0])
-    # print("Accuracy:", accuracy)
+    odor = stand.transform(odor)
+    y_pred = model.predict(odor)
+    print("Output : ", y_pred[0], "\n")
